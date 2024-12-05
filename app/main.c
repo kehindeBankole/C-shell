135531c2805f9d722151d387b59f2f4945cdcb89
kehinde <bankolek1@gmail.com>
2024-12-05 15:30:40 +0100

    [any message]

--- app/main.c
+++ app/main.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main() {
   // Uncomment this block to pass the first stage
-   printf("$ ");
-   fflush(stdout);
+    bool canRepeat = true;
+    
+    while (canRepeat) {
+        printf("$ ");
+        fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-    
-    input[strlen(input) - 1] = '\0';
-    printf("%s: command not found\n", input);
-    
+       // Wait for user input
+       char input[100];
+       fgets(input, 100, stdin);
+         
+         input[strlen(input) - 1] = '\0';
+         printf("%s: command not found\n", input);
+
+    }
   return 0;
 }
